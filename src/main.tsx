3efb22b3fadece9307069bc5b09697c8b9a5bae1
Santiago Bejarano <56372994+San7iag0@users.noreply.github.com>
2025-09-01 15:02:56 -0500

    update project config

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { store } from './store.ts'
+
+// Fonts
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+// MUI styles provider
+import { CssBaseline } from '@mui/material';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <CssBaseline />
+      < App />
+    </Provider>
   </StrictMode>,
 )
